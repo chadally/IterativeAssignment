2d0fe0c6b3ef6dc39a9cce81082212bc68f61ed2
Chad Ally <chadally@usf.edu>
2020-02-19 20:19:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IterativeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer that is between 2 and 10.");
+            int i = Convert.ToInt32(Console.ReadLine());
+            if (i % 2 == 1)
+            {
+                for (int n = 0; n < i - 1; n++)
+                    Console.WriteLine("You entered an odd number.");
+            }
+            else
+            {
+                for (int n = 0; n < i + 1; n++)
+                    Console.WriteLine("You entered an even number.");
+            }
+        }
+    }
+}
