02b60e2e2174a41f114d56764ec29e41629ffa40
Chad Ally <chadally@usf.edu>
2020-02-19 20:43:45 -0500

    Changed to even numbers first

--- Program.cs
+++ Program.cs
@@ -8,15 +8,15 @@ namespace IterativeAssignment
         {
             Console.WriteLine("Enter an integer that is between 2 and 10.");
             int i = Convert.ToInt32(Console.ReadLine());
-            if (i % 2 == 1)
-            {
-                for (int n = 0; n < i - 1; n++)
-                    Console.WriteLine("You entered an odd number.");
-            }
-            else
+            if (i % 2 == 0)
             {
                 for (int n = 0; n < i + 1; n++)
                     Console.WriteLine("You entered an even number.");
+            }
+            else
+            {
+                for (int n = 0; n < i - 1; n++)
+                    Console.WriteLine("You entered an odd number.");
             }
         }
     }
